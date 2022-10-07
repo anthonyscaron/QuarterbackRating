c2df4b259ef837eb4c1ae8b026d05e5857cdf0ac
Anthony Caron <anthonyscaron@gmail.com>
2022-10-07 08:17:51 -0500

    ReadAll method and test

--- QuarterbackRating/QuarterbackRating/Data/QuarterbackRepo.cs
+++ QuarterbackRating/QuarterbackRating/Data/QuarterbackRepo.cs
@@ -11,7 +11,11 @@ namespace QuarterbackRating.Data
     {
         private static List<Quarterback> _quarterbacks = new List<Quarterback>
         {
-            new Quarterback {Id=1, Name="Steve Young", PassAttempts=461, PassCompletions=324, PassingYards=3969, Touchdowns=35, Interceptions=10, Rating=112.8M}
+            new Quarterback {Id=1, Name="Kirk Cousins", PassAttempts=157, PassCompletions=99, PassingYards=1031, Touchdowns=6, Interceptions=4, Rating=84.1M},
+            new Quarterback {Id=2, Name="Lamar Jackson", PassAttempts=117, PassCompletions=76, PassingYards=893, Touchdowns=11, Interceptions=4, Rating=105.1M},
+            new Quarterback {Id=3, Name="Tom Brady", PassAttempts=155, PassCompletions=106, PassingYards=1058, Touchdowns=6, Interceptions=1, Rating=97.7M},
+            new Quarterback {Id=4, Name="Patrick Mahomes", PassAttempts=146, PassCompletions=97, PassingYards=1106, Touchdowns=11, Interceptions=2, Rating=108.4M},
+            new Quarterback {Id=5, Name="Justin Herbert", PassAttempts=166, PassCompletions=111, PassingYards=1250, Touchdowns=9, Interceptions=2, Rating=102.2M}
         };
 
         public List<Quarterback> ReadAll()
---
+++ QuarterbackRating/UnitTests/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuarterbackRating.Data;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void CanReadAllQuarterbacks()
+        {
+            var repo = new QuarterbackRepo();
+            var quarterbacks = repo.ReadAll();
+
+            Assert.AreEqual(5, quarterbacks.Count);
+            Assert.AreEqual(2, quarterbacks[1].Id);
+            Assert.AreEqual("Lamar Jackson", quarterbacks[1].Name);
+            Assert.AreEqual(117, quarterbacks[1].PassAttempts);
+            Assert.AreEqual(76, quarterbacks[1].PassCompletions);
+            Assert.AreEqual(893, quarterbacks[1].PassingYards);
+            Assert.AreEqual(11, quarterbacks[1].Touchdowns);
+            Assert.AreEqual(4, quarterbacks[1].Interceptions);
+            Assert.AreEqual(105.1M, quarterbacks[1].Rating);
+        }
+    }
+}
