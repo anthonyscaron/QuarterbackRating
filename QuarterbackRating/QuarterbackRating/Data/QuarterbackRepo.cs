54897fb22edbc5e9f83a38e4e3909d4ec8077f50
Anthony Caron <anthonyscaron@gmail.com>
2022-10-07 07:19:42 -0500

    Model update, repo init

---
+++ QuarterbackRating/QuarterbackRating/Data/QuarterbackRepo.cs
@@ -0,0 +1,22 @@
+﻿using QuarterbackRating.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuarterbackRating.Data
+{
+    public class QuarterbackRepo
+    {
+        private static List<Quarterback> _quarterbacks = new List<Quarterback>
+        {
+            new Quarterback {Id=1, Name="Steve Young", PassAttempts=461, PassCompletions=324, PassingYards=3969, Touchdowns=35, Interceptions=10, Rating=112.8M}
+        };
+
+        public List<Quarterback> ReadAll()
+        {
+            return _quarterbacks;
+        }
+    }
+}
--- QuarterbackRating/QuarterbackRating/Models/Quarterback.cs
+++ QuarterbackRating/QuarterbackRating/Models/Quarterback.cs
@@ -10,10 +10,11 @@ namespace QuarterbackRating.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int PassesAttempted { get; set; }
-        public int PassesCompleted { get; set; }
+        public int PassAttempts { get; set; }
+        public int PassCompletions { get; set; }
         public int PassingYards { get; set; }
         public int Touchdowns { get; set; }
         public int Interceptions { get; set; }
+        public decimal Rating { get; set; }
     }
 }
