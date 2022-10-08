82c21cdd05841791cc9b0a98ef1de769a5cfb47c
Anthony Caron <anthonyscaron@gmail.com>
2022-10-07 21:18:14 -0500

    Pop up

--- QuarterbackRating/QuarterbackRating/Views/Create.xaml.cs
+++ QuarterbackRating/QuarterbackRating/Views/Create.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -47,8 +48,18 @@ namespace QuarterbackRating.Views
             quarterback.Interceptions = Int32.Parse(Interceptions.Text);
 
             repo.Create(quarterback);
-            
+
+            var quarterbackWithRating = repo.ReadAll().LastOrDefault(q => q.Name == Name.Text);
+
+            var message = new MessageDialog(quarterbackWithRating.Name + "'s QB Rating is: " + quarterbackWithRating.Rating);
+            ShowPopUp(message);
+
             this.Frame.Navigate(typeof(MainPage));
+        }
+
+        static async void ShowPopUp(MessageDialog message)
+        {
+            await message.ShowAsync();
         }
     }
 }
