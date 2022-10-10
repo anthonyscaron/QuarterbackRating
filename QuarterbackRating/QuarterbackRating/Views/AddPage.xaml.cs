b66f1c203ae186948e17c8fe79fcbb56b9c1ed92
Anthony Caron <anthonyscaron@gmail.com>
2022-10-10 09:38:14 -0500

    Data validation

--- QuarterbackRating/QuarterbackRating/Views/AddPage.xaml.cs
+++ QuarterbackRating/QuarterbackRating/Views/AddPage.xaml.cs
@@ -18,12 +18,14 @@ namespace QuarterbackRating.Views
 
         private void CalculateButton_ItemClick(object sender, ItemClickEventArgs e)
         {
+            ResetFormStyle();
+            ToggleConfirmationAndSave(false);
+
             var errorsExist = UserInputValidation(NameBox.Text, AttemptsBox.Text, CompletionsBox.Text, YardsBox.Text, TouchdownsBox.Text, InterceptionsBox.Text);
 
             if (!errorsExist)
             {
                 ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(false, null, null, null);
-                NameBoxLabel.Foreground = new SolidColorBrush(Colors.White);
 
                 ViewModel.Quarterback.Name = NameBox.Text;
                 ViewModel.Quarterback.PassAttempts = Int32.Parse(AttemptsBox.Text);
@@ -38,15 +40,15 @@ namespace QuarterbackRating.Views
                 var checkString = ViewModel.Quarterback.Name + "'s QB Rating is: " + checkRating;
                 RatingConfirmationBox.Text = checkString;
 
-                RatingConfirmationIcon.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
-                RatingConfirmation.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
-                SaveButtonIcon.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
-                SaveButton.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                ToggleConfirmationAndSave(true);
             }        
         }
 
         private bool UserInputValidation(string name, string attempts, string completions, string yards, string touchdowns, string interceptions)
         {
+            RatingBox.Foreground = new SolidColorBrush(Colors.White);
+            RatingBox.Text = "PENDING";
+
             var errorsExist = false;
 
             if (string.IsNullOrEmpty(name))
@@ -56,7 +58,76 @@ namespace QuarterbackRating.Views
                 NameBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
             }
 
+            var attemptsIsNumber = Int32.TryParse(attempts, out int attemptsResult);
+
+            if (string.IsNullOrEmpty(attempts) || !attemptsIsNumber || attemptsResult < 1)
+            {
+                errorsExist = true;
+                ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                AttemptsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+            var completionsIsNumber = Int32.TryParse(completions, out int completionsResult);
+
+            if (string.IsNullOrEmpty(completions) || !completionsIsNumber || completionsResult < 0)
+            {
+                errorsExist = true;
+                ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                CompletionsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+            var yardsIsNumber = Int32.TryParse(yards, out int yardsResult);
+
+            if (string.IsNullOrEmpty(yards) || !yardsIsNumber || yardsResult < 0)
+            {
+                errorsExist = true;
+                ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                YardsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+            var touchdownsIsNumber = Int32.TryParse(touchdowns, out int touchdownsResult);
+
+            if (string.IsNullOrEmpty(touchdowns) || !touchdownsIsNumber || touchdownsResult < 0)
+            {
+                errorsExist = true;
+                ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                TouchdownsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+            var interceptionsIsNumber = Int32.TryParse(interceptions, out int interceptionsResult);
+
+            if (string.IsNullOrEmpty(interceptions) || !interceptionsIsNumber || interceptionsResult < 0)
+            {
+                errorsExist = true;
+                ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                InterceptionsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
             return errorsExist;
+        }
+
+        private void ToggleConfirmationAndSave(bool desiredVisibility)
+        {
+            RatingConfirmationIcon.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(desiredVisibility, null, null, null);
+            RatingConfirmation.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(desiredVisibility, null, null, null);
+            SaveButtonIcon.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(desiredVisibility, null, null, null);
+            SaveButton.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(desiredVisibility, null, null, null);
+        }
+
+        private void ResetFormStyle()
+        {
+            ErrorAlert.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(false, null, null, null);
+
+            var originalColor = new SolidColorBrush(Colors.White);
+            NameBoxLabel.Foreground = originalColor;
+            AttemptsBoxLabel.Foreground = originalColor;
+            CompletionsBoxLabel.Foreground = originalColor;
+            YardsBoxLabel.Foreground = originalColor;
+            TouchdownsBoxLabel.Foreground = originalColor;
+            InterceptionsBoxLabel.Foreground = originalColor;
+            RatingBox.Foreground = originalColor;
+            RatingBox.Text = "PENDING";
+            RatingConfirmationBox.Text = "";
         }
     }
 }
