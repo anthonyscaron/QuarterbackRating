fdca824b063e621e377f7dfad56e925a8bba60c4
Anthony Caron <anthonyscaron@gmail.com>
2022-10-09 20:04:01 -0500

    Visibility Click events

--- QuarterbackRating/QuarterbackRating/ViewModels/AddViewModel.cs
+++ QuarterbackRating/QuarterbackRating/ViewModels/AddViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using QuarterbackRating.Core.Models;
+using QuarterbackRating.Core.Services;
 
 namespace QuarterbackRating.ViewModels
 {
     public class AddViewModel : ObservableObject
     {
+        public Quarterback Quarterback = new Quarterback();
+        public RatingCalculator Calculator = new RatingCalculator();
+        public QuarterbackRepository Repository = new QuarterbackRepository();
+        public BooleanToVisibilityConverter Converter = new BooleanToVisibilityConverter();
+
         public AddViewModel()
         {
         }
--- QuarterbackRating/QuarterbackRating/Views/AddPage.xaml.cs
+++ QuarterbackRating/QuarterbackRating/Views/AddPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using QuarterbackRating.Core.Services;
 using QuarterbackRating.ViewModels;
 
 using Windows.UI.Xaml.Controls;
@@ -13,6 +13,26 @@ namespace QuarterbackRating.Views
         public AddPage()
         {
             InitializeComponent();
+        }
+
+        private void CalculateButton_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            ViewModel.Quarterback.Name = NameBox.Text;
+            ViewModel.Quarterback.PassAttempts = Int32.Parse(AttemptsBox.Text);
+            ViewModel.Quarterback.PassCompletions = Int32.Parse(CompletionsBox.Text);
+            ViewModel.Quarterback.PassingYards = Int32.Parse(YardsBox.Text);
+            ViewModel.Quarterback.Touchdowns = Int32.Parse(TouchdownsBox.Text);
+            ViewModel.Quarterback.Interceptions = Int32.Parse(InterceptionsBox.Text);
+
+            var checkRating = ViewModel.Calculator.CalculateRating(ViewModel.Quarterback);
+            RatingBox.Text = checkRating.ToString();
+            var checkString = ViewModel.Quarterback.Name + "'s QB Rating is: " + checkRating;
+            RatingConfirmationBox.Text = checkString;
+
+            RatingConfirmationIcon.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+            RatingConfirmation.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+            SaveButtonIcon.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+            SaveButton.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
         }
     }
 }
