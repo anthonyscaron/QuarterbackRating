7f8e9d72f54e45dafbca64beb4cd97c44e72e163
Anthony Caron <anthonyscaron@gmail.com>
2022-10-12 10:42:19 -0500

    Specific error messages for user input validation

--- QuarterbackRating/QuarterbackRating/Views/AddPage.xaml.cs
+++ QuarterbackRating/QuarterbackRating/Views/AddPage.xaml.cs
@@ -101,40 +101,105 @@ namespace QuarterbackRating.Views
                 else if (completionsResult > attemptsResult)
                 {
                     CompletionsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
-                    CompletionsError.Text = "- Pass Completions cannot be greater than attempts";
+                    CompletionsError.Text = "- Pass Completions cannot be greater than Pass Attempts";
                 }
             }
 
             var yardsIsNumber = Int32.TryParse(yards, out int yardsResult);
             var maxNegativeYards = completionsResult * -99;
             var maxPositiveYards = completionsResult * 99;
-
             // yardsResult < maxNegativeYards || yardsResult > maxPositiveYards, The max yards any pass can go for are
             // between -99 and 99 per completion depending on the specific field placement at the time of the completion.
             if (string.IsNullOrEmpty(yards) || !yardsIsNumber || yardsResult < maxNegativeYards || yardsResult > maxPositiveYards)
             {
                 errorsExist = true;
                 YardsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+
+                if (string.IsNullOrEmpty(yards))
+                {
+                    YardsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    YardsError.Text = "- Passing Yards cannot be blank";
+                }
+                else if (!yardsIsNumber)
+                {
+                    YardsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    YardsError.Text = "- Passing Yards must be a number";
+                }
+                else if (yardsResult < maxNegativeYards)
+                {
+                    YardsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    YardsError.Text = "- Passing Yards cannot be less than -99 yards per Pass Attempt";
+                }
+                else if (yardsResult > maxPositiveYards)
+                {
+                    YardsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    YardsError.Text = "- Passing Yards cannot be greater than 99 yards per Pass Attempt";
+                }
             }
 
             var touchdownsIsNumber = Int32.TryParse(touchdowns, out int touchdownsResult);
-
             // touchdownsResult > completionsResult, Can't have more touchdowns than completions.
             // touchdownsResult > yardsResult, Can't have more touchdowns than yards because 1 touchdown would be for at least 1 yard.
             if (string.IsNullOrEmpty(touchdowns) || !touchdownsIsNumber || touchdownsResult < 0 || touchdownsResult > completionsResult || touchdownsResult > yardsResult)
             {
                 errorsExist = true;
                 TouchdownsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+
+                if (string.IsNullOrEmpty(touchdowns))
+                {
+                    TouchdownsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    TouchdownsError.Text = "- Passing Touchdowns cannot be blank";
+                }
+                else if (!touchdownsIsNumber)
+                {
+                    TouchdownsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    TouchdownsError.Text = "- Passing Touchdowns must be a number";
+                }
+                else if (touchdownsResult < 0)
+                {
+                    TouchdownsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    TouchdownsError.Text = "- Passing Touchdowns cannot be less than 0";
+                }
+                else if (touchdownsResult > completionsResult)
+                {
+                    TouchdownsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    TouchdownsError.Text = "- Passing Touchdowns cannot be greater than Pass Completions";
+                }
+                else if (touchdownsResult > yardsResult)
+                {
+                    TouchdownsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    TouchdownsError.Text = "- Passing Touchdowns cannot be greater than Passing Yards";
+                }
             }
 
             var interceptionsIsNumber = Int32.TryParse(interceptions, out int interceptionsResult);
             var maxInterceptions = attemptsResult - completionsResult;
-
             // interceptionsResult > maxInterceptions, Can't have more interceptions than non-completion attempts.
             if (string.IsNullOrEmpty(interceptions) || !interceptionsIsNumber || interceptionsResult < 0 || interceptionsResult > maxInterceptions)
             {
                 errorsExist = true;
                 InterceptionsBoxLabel.Foreground = new SolidColorBrush(Colors.Red);
+
+                if (string.IsNullOrEmpty(interceptions))
+                {
+                    InterceptionsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    InterceptionsError.Text = "- Interceptions cannot be blank";
+                }
+                else if (!interceptionsIsNumber)
+                {
+                    InterceptionsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    InterceptionsError.Text = "- Interceptions must be a number";
+                }
+                else if (interceptionsResult < 0)
+                {
+                    InterceptionsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    InterceptionsError.Text = "- Interceptions cannot be less than 0";
+                }
+                else if (interceptionsResult > maxInterceptions)
+                {
+                    InterceptionsError.Visibility = (Windows.UI.Xaml.Visibility)ViewModel.Converter.Convert(true, null, null, null);
+                    InterceptionsError.Text = "- Interceptions cannot be greater than Non-Completion Pass Attempts";
+                }
             }
 
             if (errorsExist)
